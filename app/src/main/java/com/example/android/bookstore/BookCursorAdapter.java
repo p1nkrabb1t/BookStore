e780b2df64b5740fe9711dfe490003346da19b8b
p1nkrabb1t <pinkcyberbunny@gmail.com>
2019-01-24 18:53:03 +0000

    Add BookCursorAdapter class (not yet linked)

---
+++ app/src/main/java/com/example/android/bookstore/BookCursorAdapter.java
@@ -0,0 +1,58 @@
+package com.example.android.bookstore;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.android.bookstore.data.BookContract;
+
+public class BookCursorAdapter extends CursorAdapter {
+
+    public BookCursorAdapter(Context context, Cursor c, int flags) {
+        super(context, c, flags);
+    }
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        return LayoutInflater.from(context).inflate(R.layout.item_book, parent, false);
+
+    }
+
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+
+        //find the relevant views
+        TextView titleTextView = (TextView) view.findViewById(R.id.tv_title);
+        TextView authorTextView = (TextView) view.findViewById(R.id.tv_author);
+        TextView priceTextView = (TextView) view.findViewById(R.id.tv_price);
+        TextView quantityTextView = (TextView) view.findViewById(R.id.tv_quantity);
+
+        //find the columns containing relevant info
+        int titleColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_NAME);
+        int authorColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_AUTHOR);
+        int priceColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_PRICE);
+        int quantityColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_QUANTITY);
+
+        //read the data from the cursor for the current item
+        String titleData = cursor.getString(titleColumnIndex);
+        String authorData = cursor.getString(authorColumnIndex);
+        String priceData = cursor.getString(priceColumnIndex);
+        String quantityData = cursor.getString(quantityColumnIndex);
+
+        if (TextUtils.isEmpty(quantityData)) {
+            quantityData = "OUT OF STOCK";
+        }
+
+
+        //feed the data collected into the relevant views
+        titleTextView.setText(titleData);
+        authorTextView.setText(authorData);
+        priceTextView.setText(priceData);
+        quantityTextView.setText(quantityData);
+    }
+}
---
+++ app/src/main/java/com/example/android/bookstore/data/BookProvider.java
@@ -0,0 +1,142 @@
+package com.example.android.bookstore.data;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.example.android.bookstore.data.BookContract.BookEntry;
+
+public class BookProvider extends ContentProvider {
+
+    private static final int BOOKS = 1;
+    private static final int BOOK = 2;
+
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+
+    static {
+        sUriMatcher.addURI(BookContract.CONTENT_AUTHORITY, BookContract.PATH_BOOKS, BOOKS);
+        sUriMatcher.addURI(BookContract.CONTENT_AUTHORITY, BookContract.PATH_BOOKS + "/#", BOOK);
+    }
+
+
+    //create instance of BookDbhelper
+    private BookDbHelper mBookHelper;
+
+    @Override
+    public boolean onCreate() {
+        mBookHelper = new BookDbHelper(getContext());
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+
+        SQLiteDatabase db = mBookHelper.getReadableDatabase();
+        Cursor cursor;
+        int match = sUriMatcher.match(uri);
+
+        switch (match) {
+            //to handle queries on the whole table of books
+            case BOOKS:
+                cursor = db.query(BookEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+
+            //to handle queries on a single book entry in the database
+            case BOOK:
+                selection = BookEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                cursor = db.query(BookEntry.TABLE_NAME, projection, selection, selectionArgs,
+                        null, null, sortOrder);
+                break;
+            //exception thrown if uri represents neither single item or table (invalid uri)
+            default:
+                throw new IllegalArgumentException(uri + " is not recognised");
+        }
+
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
+    }
+
+
+    @Override
+    public String getType(@NonNull Uri uri) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case BOOKS:
+                return BookEntry.CONTENT_LIST_TYPE;
+            case BOOK:
+                return BookEntry.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalStateException(uri + " is not recognised");
+        }
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case BOOKS:
+                return insertBook(uri, contentValues);
+            default:
+                throw new IllegalArgumentException(uri + "could not be added");
+        }
+    }
+
+    private Uri insertBook(Uri uri, ContentValues values) {
+
+        // Check that the name is not null
+        String name = values.getAsString(BookEntry.COLUMN_NAME);
+        if (name == null || name.length() == 0) {
+            throw new IllegalArgumentException("Title not found");
+        }
+
+        // Check that the price is valid
+        Integer price = values.getAsInteger(BookEntry.COLUMN_PRICE);
+        if (price == null || price < 0) {
+            throw new IllegalArgumentException("Price not valid");
+        }
+
+        // Check that the quantity is valid
+        Integer quantity = values.getAsInteger(BookEntry.COLUMN_QUANTITY);
+        if (quantity == null || quantity < 0) {
+            throw new IllegalArgumentException("Quantity not valid");
+        }
+
+        // Check that the supplier field is not null
+        String supplier = values.getAsString(BookEntry.COLUMN_SUPPLIER_NAME);
+        if (supplier == null || supplier.length() == 0) {
+            throw new IllegalArgumentException("Supplier entry required");
+        }
+
+        SQLiteDatabase database = mBookHelper.getWritableDatabase();
+        long id = database.insert(BookEntry.TABLE_NAME, null, values);
+
+        if (id == -1) {
+            //not valid entry
+            return null;
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+        return ContentUris.withAppendedId(uri, id);
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        return 0;
+    }
+}
