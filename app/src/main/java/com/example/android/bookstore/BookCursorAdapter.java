f192c76676120f46923a357d4f86c84952eef609
p1nkrabb1t <pinkcyberbunny@gmail.com>
2019-02-03 18:53:05 +0000

    make in line sale button clickable as well as book list item (testing only, sale logic not yet added), fix logic to display correct stock message

--- app/src/main/java/com/example/android/bookstore/BookCursorAdapter.java
+++ app/src/main/java/com/example/android/bookstore/BookCursorAdapter.java
@@ -2,16 +2,18 @@ package com.example.android.bookstore;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.bookstore.data.BookContract;
 
 public class BookCursorAdapter extends CursorAdapter {
+
 
     public BookCursorAdapter(Context context, Cursor c, int flags) {
         super(context, c, 0);
@@ -31,6 +33,7 @@ public class BookCursorAdapter extends CursorAdapter {
         TextView authorTextView = (TextView) view.findViewById(R.id.tv_author);
         TextView priceTextView = (TextView) view.findViewById(R.id.tv_price);
         TextView quantityTextView = (TextView) view.findViewById(R.id.tv_quantity);
+        Button saleButton = (Button) view.findViewById(R.id.btn_sale);
 
         //find the columns containing relevant info
         int titleColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_NAME);
@@ -41,18 +44,29 @@ public class BookCursorAdapter extends CursorAdapter {
         //read the data from the cursor for the current item
         String titleData = cursor.getString(titleColumnIndex);
         String authorData = cursor.getString(authorColumnIndex);
-        String priceData = cursor.getString(priceColumnIndex);
-        String quantityData = cursor.getString(quantityColumnIndex);
-
-        if (TextUtils.isEmpty(quantityData)) {
-            quantityData = "OUT OF STOCK";
-        }
+        Integer priceData = cursor.getInt(priceColumnIndex);
+        Integer quantityData = cursor.getInt(quantityColumnIndex);
 
 
         //feed the data collected into the relevant views
         titleTextView.setText(titleData);
         authorTextView.setText(authorData);
-        priceTextView.setText("£" + priceData);
-        quantityTextView.setText("In Stock: " + quantityData);
+        priceTextView.setText("RRP £" + priceData);
+
+        //display relevant stock message depending if item is in stock or not
+        if (quantityData == null || quantityData == 0) {
+            quantityTextView.setText("OUT OF STOCK");
+        } else {
+            quantityTextView.setText("In Stock: " + quantityData);
+        }
+
+        //set click listener to sale button
+        View.OnClickListener sale = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "test", Toast.LENGTH_SHORT).show();
+            }
+        };
+        saleButton.setOnClickListener(sale);
     }
 }
