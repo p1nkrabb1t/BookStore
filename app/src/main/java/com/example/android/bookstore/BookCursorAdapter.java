e56c99dbc5454ec59d69323153ce1bec5bce621c
p1nkrabb1t <pinkcyberbunny@gmail.com>
2019-02-06 19:54:30 +0000

    Add working in-line 'sale' button to make quantity reduce by 1 of any item where sale button is clicked

--- app/src/main/java/com/example/android/bookstore/BookCursorAdapter.java
+++ app/src/main/java/com/example/android/bookstore/BookCursorAdapter.java
@@ -3,7 +3,6 @@ package com.example.android.bookstore;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,15 +10,11 @@ import android.widget.Button;
 import android.widget.CursorAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.app.LoaderManager;
+
 import com.example.android.bookstore.data.BookContract;
 import com.example.android.bookstore.data.BookContract.BookEntry;
-import com.example.android.bookstore.data.BookDbHelper;
-import com.example.android.bookstore.data.BookProvider;
 
 public class BookCursorAdapter extends CursorAdapter {
-
-//    Integer quantityData;
 
 
     public BookCursorAdapter(Context context, Cursor c, int flags) {
@@ -29,31 +24,30 @@ public class BookCursorAdapter extends CursorAdapter {
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
         return LayoutInflater.from(context).inflate(R.layout.item_book, parent, false);
-
     }
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, final Context context, final Cursor cursor) {
+
 
         //find the relevant views
         TextView titleTextView = (TextView) view.findViewById(R.id.tv_title);
         TextView authorTextView = (TextView) view.findViewById(R.id.tv_author);
         TextView priceTextView = (TextView) view.findViewById(R.id.tv_price);
-        TextView quantityTextView = (TextView) view.findViewById(R.id.tv_quantity);
-        Button saleButton = (Button) view.findViewById(R.id.btn_sale);
+        final TextView quantityTextView = (TextView) view.findViewById(R.id.tv_quantity);
+
 
         //find the columns containing relevant info
         int titleColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_NAME);
         int authorColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_AUTHOR);
         int priceColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_PRICE);
-        int quantityColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_QUANTITY);
+        final int quantityColumnIndex = cursor.getColumnIndex(BookContract.BookEntry.COLUMN_QUANTITY);
 
         //read the data from the cursor for the current item
         String titleData = cursor.getString(titleColumnIndex);
         String authorData = cursor.getString(authorColumnIndex);
         Integer priceData = cursor.getInt(priceColumnIndex);
-        Integer quantityData = cursor.getInt(quantityColumnIndex);
-
+        final Integer quantityData = cursor.getInt(quantityColumnIndex);
 
         //feed the data collected into the relevant views
         titleTextView.setText(titleData);
@@ -68,23 +62,33 @@ public class BookCursorAdapter extends CursorAdapter {
 
         }
 
+        Button saleButton = (Button) view.findViewById(R.id.btn_sale);
 
-//        BookDbHelper bookHelper = new BookDbHelper(view.getContext());
-//        SQLiteDatabase database = bookHelper.getWritableDatabase();
-//        ContentValues values = new ContentValues();
-
-        //set click listener to sale button
-        View.OnClickListener sale = new View.OnClickListener() {
+        final int position = cursor.getPosition();
+        saleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                quantityData = quantityData - 1;
-//                TextView quantityTextView = (TextView) view.findViewById(R.id.tv_quantity);
-//                quantityTextView.setText("In Stock: " + quantityData);
-                Toast.makeText(view.getContext(), "Book Sold, stock updated", Toast.LENGTH_SHORT).show();
+                cursor.moveToPosition(position);
+                Integer quantityData = cursor.getInt(quantityColumnIndex);
+                quantityData = quantityData - 1;
+
+                int idColumnIndex = cursor.getColumnIndex(BookEntry._ID);
+                String[] id = {String.valueOf(cursor.getInt(idColumnIndex))};
+
+                ContentValues values = new ContentValues();
+
+                String selection = BookEntry._ID + "=?";
+
+                values.put(BookEntry.COLUMN_QUANTITY, quantityData);
+                context.getContentResolver().update(BookEntry.CONTENT_URI, values, selection, id);
+                cursor.close();
+
+                Toast.makeText(view.getContext(), "Book Sold, stock updated at " + ", new qty= " + quantityData, Toast.LENGTH_SHORT).show();
 
             }
-        };
 
-        saleButton.setOnClickListener(sale);
+        });
+
+
     }
 }
